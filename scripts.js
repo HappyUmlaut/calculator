5c39e4cb62a6e230557cdbe93dea05ff0e252fbb
Leobardo Arguelles <leobardomed@gmail.com>
2020-08-26 19:50:41 -0700

    Se cambio el estilo de la calculadora. Se terminaron de agregar las funciones: Suma, resta, division, multiplicacion, porcentaje, raiz cuadrada, memoria

--- scripts.js
+++ scripts.js
@@ -1,54 +1,115 @@
 let result = 0;
 let num1 = 0;
 let num2 = 0;
-let firstTimeFlag = true;
+let memoryFlag = false;
+let finalResult = false; // Becomes true if 'enter' or 'equals' has been entered, because it is the final result.
 let previousOperation = '';
+let memory = 0;
+
+function addMemory(){
+    memory += +document.querySelector('#bottomRight').textContent;
+    document.querySelector('#topLeft').textContent = 'M';
+}
+
+function substractMemory(){
+    memory -= +document.querySelector('#bottomRight').textContent;
+    document.querySelector('#topLeft').textContent = 'M';
+}
+
+function clearMemory(){
+    memory = 0;
+    document.querySelector('#topLeft').textContent = '';
+}
+
+function retrieveMemory(){
+    document.querySelector('#bottomRight').textContent = memory;
+}
 
 function add(num1,num2){
-    return num1+num2;
+    return (num1+num2).toFixed(2);
 }
 
 function substract(num1,num2){
-    return num1-num2;
+    return (num1-num2).toFixed(2);
 }
 
 function multiply(num1,num2){
-    return num1*num2;
+    return (num1*num2).toFixed(2);
 }
 
 function divide(num1,num2){
-    return num1/num2;
+    return (num1/num2).toFixed(2);
+}
+
+function sqrt(num){
+    let sqrtResult = Math.sqrt(num).toFixed(2);
+    if(sqrtResult.toString().endsWith('00')){
+        sqrtResult = sqrtResult.toString().substring(0,sqrtResult.toString().length-3);
+    }
+    document.querySelector('#bottomRight').textContent = sqrtResult;
 }
 
 function operate(operator){
     updateNumbers();
     switch(operator){
         case '+':
-            return add(num1,num2);
+            result = add(num1,num2);
             break;
         case '-':
-            return substract(num1,num2);
+            result = substract(num1,num2);
             break;
         case '*':
-            return multiply(num1,num2);
+            result = multiply(num1,num2);
             break;
         case '/':
-            return divide(num1,num2);
+            if(document.querySelector('#bottomRight').textContent==='0'){
+                result = "ERROR: Can't divide by zero";
+            }else{
+                result = divide(num1,num2);
+            }
             break;
+        default:
+            break
     }
+    let stringResult = result.toString()
+    if(stringResult.endsWith('00')){
+        stringResult = stringResult.substring(0,stringResult.length-3);
+    }
+    return stringResult;
 }
 
-function clear(){
-    document.querySelectorAll('p').forEach(element => element.textContent='');
+function clearScreen(){
+    document.querySelectorAll('p').forEach(element => {
+        if(element.id==='topLeft'){
+            return;
+        }
+        element.textContent='';
+    });
     previousOperation = '';
-    result = 0;
     document.querySelector('#bottomRight').textContent = '0';
 }
 
+function cleanResult(){
+    result = 0;
+}
+
+function changeSign(){
+    bottomLine = document.querySelector('#bottomRight');
+    if(!bottomLine.textContent.includes('-')){
+        bottomLine.textContent = '-'.concat(bottomLine.textContent);
+    }else{
+        bottomLine.textContent = bottomLine.textContent.substring(1);
+    }
+}
+
 function moveLinesUp(){
-    //document.querySelector('#topRight').textContent = document.querySelector('#centerRight').textContent;
     document.querySelector('#centerRight').textContent = document.querySelector('#bottomRight').textContent;
-    //document.querySelector('#centerLeft').textContent = document.querySelector('#bottomLeft').textContent;
+    document.querySelector('#bottomRight').textContent = 0;
+}
+
+function percentage(){
+    bottomLine = document.querySelector('#bottomRight');
+    bottomLine.textContent = bottomLine.textContent/100;
 }
 
 function updateNumbers(){
@@ -62,47 +123,98 @@ function updateNumbers(){
 
 function writeOnScreen(text){
     let bottomLine = document.querySelector('#bottomRight');
-    let centerLine = document.querySelector('#centerRight');
     let bottomLeft = document.querySelector('#bottomLeft');
-    if(text.search(/\d/)===0){
+    let centerLine = document.querySelector('#centerRight');
+    if(text.search(/\d/)===0 || text===('.')){
+        if(finalResult){
+            bottomLine.textContent = '0';
+            result = 0;
+            finalResult = false;
+        }
         if(bottomLine.textContent==='0'){
             bottomLine.textContent = text;
-        } else{
+        } else if(!bottomLine.textContent.includes('.') && text==='.'){
+            bottomLine.textContent += text;
+        }else if(text!=='.'){
             bottomLine.textContent += text;
         }
     } else if(previousOperation==='+'||previousOperation==='-'|| 
             previousOperation==='/'||previousOperation==='*'){
-                result = operate(text);
+                result = operate(previousOperation);
+                let stringResult = result.toString()
+                if(stringResult.endsWith('00')){
+                    bottomLine.textContent = stringResult.substring(0,stringResult.length-3);
+                }else{
+                    bottomLine.textContent = result;
+                }
                 moveLinesUp();
-                bottomLine.textContent = 0;
-                // if(firstTimeFlag){
-                //     bottomLine.textContent = 0;
-                //     firstTimeFlag = false;
-                // } else{
-                //     centerLine.textContent = result;
-                // }
                 document.querySelector('#bottomLeft').textContent = text;
+                if(centerLine.textContent.includes('ERROR')){
+                    clearScreen();
+                    bottomLine.textContent = result;
+                }
             }
-    if (text==='+' || text==='-' || text==='*' || text==='/'){
-        console.log(previousOperation);
+    if(bottomLeft.textContent==='' && (text==='+' || text==='-' || text==='*' || 
+        text==='/') && !bottomLine.textContent.includes('ERROR')){
         previousOperation = text;
-        console.log(previousOperation);
         bottomLeft.textContent = text;
         moveLinesUp();
-        bottomLine.textContent = 0;
     } else{
-        previousOperation = document.querySelector('#bottomLeft').textContent;
+        previousOperation = bottomLeft.textContent;
     }
+}
+
+function del(){
+    let line = document.querySelector('#bottomRight');
+    line.textContent = line.textContent.substring(0,line.textContent.length-1);
 }
 
 function buttonClick(element){
     if(element.id==='C'){
-        clear();
+        clearScreen();
+        cleanResult();
     } else if(element.id==='equals'){
-        clear();
-        writeOnScreen(result.toString());
+        result = operate(document.querySelector('#bottomLeft').textContent);
+        clearScreen();
+        document.querySelector('#bottomRight').textContent = (result.toString());
+        finalResult = true;
+    }else if(element.id==='delete'){
+        del();
+    }else if(element.id==='sign'){
+        changeSign();
+    }else if(element.id==='percentage'){
+        percentage();
+    }else if(element.id==='sqrt'){
+        sqrt(document.querySelector('#bottomRight').textContent);
+    }else if(element.id==='Mc'){
+        clearMemory();
+    }else if(element.id==='Mr'){
+        retrieveMemory();
+    }else if(element.id==='M+'){
+        addMemory();
+    }else if(element.id==='M-'){
+        substractMemory();
     }else{
         writeOnScreen(element.textContent);
+    }
+}
+
+function keyboardPress(key){
+    console.log(key);
+    switch(key){
+        case 'Enter':
+            result = operate(document.querySelector('#bottomLeft').textContent);
+            clearScreen();
+            document.querySelector('#bottomRight').textContent = (result.toString());
+            finalResult = true;
+            break;
+        case 'Backspace':
+            del();
+            break;
+        default:
+            writeOnScreen(key);
+            break;
+
     }
 }
 
@@ -111,20 +223,20 @@ function generateButtons(){
                         '7','8','9','/','√',
                         '4','5','6','*','%',
                         '1','2','3','-','+','0','.',
-                        '+/-','=']
+                        '+/-','=','Del']
     const ids = ['Mc','Mr','M+','M-','C',
                 'seven','eight','nine','division','sqrt',
                 'four','five','six','multiplication','percentage',
                 'one','two','three','substraction','addition','zero','decimal',
-                'sign','equals']
+                'sign','equals','delete']
     let myButton;
     let buttonsArea = document.querySelector('.wrapper');
-    for(let i = 0; i<24; i++){
+    for(let i = 0; i<ids.length; i++){
             myButton = document.createElement('button');
             if(ids[i]==='Mc'||ids[i]==='Mr'||ids[i]==='M-'||ids[i]==='C'||
                 ids[i]==='division'||ids[i]==='sqrt'||ids[i]==='multiplication'||
                 ids[i]==='percentage'||ids[i]==='substraction'||ids[i]==='addition'||
-                ids[i]==='equals'||ids[i]==='M+'){
+                ids[i]==='equals'||ids[i]==='M+'|| ids[i]==='delete'){
                 myButton.className = 'special-buttons';
                         } else {
                             myButton.className = 'number-buttons';
@@ -137,4 +249,4 @@ function generateButtons(){
 }
 
 generateButtons();
-
+window.addEventListener('keydown',(e) => keyboardPress(e.key));
